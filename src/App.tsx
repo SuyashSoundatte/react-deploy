4173516535a648fa1742171fe251789eb434ab98
SuyashSoundatte <suyashsoundatte7@gmail.com>
2025-06-04 13:10:39 +0530

    new feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import './App.css'
 
 function App() {
 
+  const hello = () =>{
+    alert("hello");
+  }
+
   return (
     <>
       <div className='flex bg-[#212121] text-white text-4xl h-screen w-full justify-center items-center'>
         <h1>hello world</h1>
+        <button onClick={hello}>click me</button>
       </div>
     </>
   )
