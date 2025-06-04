0a52c239534aee418a558b9e75649ea2320e98bf
SuyashSoundatte <suyashsoundatte7@gmail.com>
2025-06-04 13:21:00 +0530

    feat2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css'
 
 function App() {
-
-  const hello = () =>{
+  const hello = () => {
     alert("hello");
   }
 
   return (
     <>
-      <div className='flex bg-[#212121] text-white text-4xl h-screen w-full justify-center items-center'>
+      <div className='flex flex-col bg-[#212121] text-white text-4xl h-screen w-full justify-center items-center gap-8'>
         <h1>hello world</h1>
-        <button onClick={hello}>click me</button>
+        <div className='flex items-center'>
+          <button
+            className='bg-blue-500 hover:bg-blue-600 active:bg-blue-700 transition-colors duration-300 p-3 rounded-3xl shadow-lg'
+            onClick={hello}
+          >
+            click me
+          </button>
+        </div>
       </div>
     </>
   )
