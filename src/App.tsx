6dbe1e26f46ae481bfff7776eed602be5bf340ff
SuyashSoundatte <suyashsoundatte7@gmail.com>
2025-06-04 12:48:36 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex bg-[#212121] text-white text-4xl h-screen w-full justify-center items-center'>
+        <h1>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
